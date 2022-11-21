dcd6289b47c7b183ff7dee38ecc48068f74b9b11
YADIN PAULO MANRIQUE MARQUEZ <ypmanrique2@misena.edu.co>
2022-11-21 15:51:02 -0500

    first commit

---
+++ src/br/com/alura/Ejemplo7.java
@@ -0,0 +1,26 @@
+package br.com.alura;
+
+import br.com.alura.model.Aula;
+import br.com.alura.model.Curso;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Ejemplo7 {
+
+    public static void main(String[] args) {
+
+        Curso curso1 = new Curso("Java",30) ;
+        curso1.addAula(new Aula("ArrayList"));
+        curso1.addAula(new Aula("List"));
+        curso1.addAula(new Aula("LinkedList"));
+        curso1.addAula(new Aula("Inmutable"));
+
+        List<Aula> aulaList = curso1.getAulaList();
+
+        ArrayList<Curso> cursos = new ArrayList<>();
+        cursos.add(curso1);
+
+        System.out.println(aulaList);
+    }
+}
